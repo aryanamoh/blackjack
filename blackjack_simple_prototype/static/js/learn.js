86e5f755a2f40840540ed2c2e96578ff654df5de
Diego Rivas Lazala <dwr2118@columbia.edu>
2024-04-15 14:28:24 -0400

    Commented out learn.js skeleton code & added comment to quiz.html for quiz template

--- blackjack_simple_prototype/static/js/learn.js
+++ blackjack_simple_prototype/static/js/learn.js
@@ -1,45 +1,46 @@
 // Skeleton javascript functions from people website 
-var displayNames = function(data){
-    //empty old data
-    $("#people_container").empty()
+// var displayNames = function(data){
+//     //empty old data
+//     $("#people_container").empty()
 
-    //insert all new data
-    $.each(data, function(i, datum){
-        var new_name= $("<div>"+datum["name"]+"</div>")
-        $("#people_container").append(new_name)
-    })
-}
+//     //insert all new data
+//     $.each(data, function(i, datum){
+//         var new_name= $("<div>"+datum["name"]+"</div>")
+//         $("#people_container").append(new_name)
+//     })
+// }
 
 var saveName = function(name){
-    var data_to_save = {"name": name}         
-    $.ajax({
-        type: "POST",
-        url: "add_name",                
-        dataType : "json",
-        contentType: "application/json; charset=utf-8",
-        data : JSON.stringify(data_to_save),
-        success: function(result){
-            var all_data = result["data"]
-            data = all_data
-            displayNames(data)
-        },
-        error: function(request, status, error){
-            console.log("Error");
-            console.log(request)
-            console.log(status)
-            console.log(error)
-        }
-    });
+    // var data_to_save = {"name": name}         
+    // $.ajax({
+    //     type: "POST",
+    //     url: "add_name",                
+    //     dataType : "json",
+    //     contentType: "application/json; charset=utf-8",
+    //     data : JSON.stringify(data_to_save),
+    //     success: function(result){
+    //         var all_data = result["data"]
+    //         data = all_data
+    //         displayNames(data)
+    //     },
+    //     error: function(request, status, error){
+    //         console.log("Error");
+    //         console.log(request)
+    //         console.log(status)
+    //         console.log(error)
+    //     }
+    // });
 }
 
 
 $(document).ready(function(){
+    // Old skeleton code
     //when the page loads, display all the names
-    displayNames(data)                        
+    // displayNames(data)                        
 
-    $("#submit_name").click(function(){                
-        var name = $("#new_name").val()
-        saveName(name)
+    // $("#submit_name").click(function(){                
+    //     var name = $("#new_name").val()
+    //     saveName(name)
 
-    })
+    // })
 })
