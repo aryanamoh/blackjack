0e9940d26199ccce4b4829bf52514be6accbc063
Aryana Mohammadi <am5723@barnard.edu>
2024-04-16 11:36:08 -0400

    re-structure data

---
+++ blackjack_simple_prototype/lessons.py
@@ -0,0 +1,45 @@
+# Usage: lessons[<Module ID>][<Lesson ID>]
+
+lessons = {  
+    "1": {
+        "1":{
+            "lesson_id": "1",
+            "title": "What is blackjack and how does a player win or lose?",
+            "media":"https://www.blackjackapprenticeship.com/wp-content/uploads/2018/08/how-do-you-play-blackjack.jpg",
+            "media_alt":"Lesson 1 media alt text",
+            "text": "Goal of blackjack is to beat the dealer.....",
+            "next_lesson": "2",
+            "start_time":0
+        },
+        "2":{
+            "lesson_id": "2",
+            "title": "What do the cards mean?....",
+            "media":"https://www.blackjackapprenticeship.com/wp-content/uploads/2018/08/how-to-play-blackjack-card-values-min.jpg",
+            "media_alt":"Lesson 2 media alt text",
+            "text": "Ace can be of value 1 or 11",
+            "next_lesson": "end",
+            "start_time":0
+        }
+    },
+    
+    "2": {
+        "1":{
+            "lesson_id": "1",
+            "title": "What is blackjack and how does a player win or lose?",
+            "media":"https://www.blackjackapprenticeship.com/wp-content/uploads/2018/08/how-do-you-play-blackjack.jpg",
+            "media_alt":"Lesson 1 media alt text",
+            "text": "Goal of blackjack is to beat the dealer.....",
+            "next_lesson": "2",
+            "start_time":0
+        },
+        "2":{
+            "lesson_id": "2",
+            "title": "What do the cards mean?....",
+            "media":"https://www.blackjackapprenticeship.com/wp-content/uploads/2018/08/how-to-play-blackjack-card-values-min.jpg",
+            "media_alt":"Lesson 2 media alt text",
+            "text": "Ace can be of value 1 or 11",
+            "next_lesson": "end",
+            "start_time":0
+        }
+    }
+}
---
+++ blackjack_simple_prototype/questions.py
@@ -0,0 +1,110 @@
+# Usage: lessons[<Module ID>][<Question ID>]
+
+quiz_questions = {  
+    "1": {
+        "1":{
+            "quiz_id": "1",
+            "quiz_question": "What are the possible values of this hand?",
+            "media":"https://decksandstacks.com/wp-content/uploads/2023/07/Example-of-a-soft-hand-in-Blackjack.png",
+            "media_alt":"Quiz question 1 media alt text",
+            "panswers": ["16 and 6", "15 and 6", "16 only", "15 only"],
+            "answer": 0,
+            "next_question": "2",
+            "client_response":""
+        },
+        "2":{
+            "quiz_id": "2",
+            "quiz_question": "What is the term for playing two hands when you're dealt two of the same card?",
+            "media":"https://www.playojo.com/blog/wp-content/uploads/2020/11/1-3-768x509.jpg",
+            "media_alt":"Quiz question 2 media alt text",
+            "panswers": ["Breaking", "Dividing", "Splitting", "Seperating"],
+            "answer": 2,
+            "next_question": "3",
+            "client_response":""
+        },
+        "3":{
+            "quiz_id": "3",
+            "quiz_question": "When is it good to take insurance?",
+            "media":"/static/assets/images/insurance.png",
+            "media_alt":"Quiz question 2 media alt text",
+            "panswers": ["Always take insurance", "Never take insurance", "Only if you have a bad hand", "When you're caught in a losing streak"],
+            "answer": 1,
+            "next_question": "4",
+            "client_response":""
+        },
+        "4":{
+            "quiz_id": "4",
+            "quiz_question": "What does a soft hand mean?",
+            "media":"/static/assets/images/insurance.png",
+            "media_alt":"Quiz question 2 media alt text",
+            "panswers": ["A hand with no face cards", "A bad hand", "A hand with an ace", "A hand with two identical cards"],
+            "answer": 2,
+            "next_question": "5",
+            "client_response":""
+        },
+        "5":{
+            "quiz_id": "5",
+            "quiz_question": "Should you surrender?",
+            "media":"/static/assets/images/surrender.png",
+            "media_alt":"Quiz question 2 media alt text",
+            "panswers": ["No, I should hit", "No, I should stand", "No, I should double", "Yes"],
+            "answer": 3,
+            "next_question": "6",
+            "client_response":""
+        }
+    
+    },
+    
+    "2": {
+        "1":{
+            "quiz_id": "1",
+            "quiz_question": "What are the possible values of this hand?",
+            "media":"https://decksandstacks.com/wp-content/uploads/2023/07/Example-of-a-soft-hand-in-Blackjack.png",
+            "media_alt":"Quiz question 1 media alt text",
+            "panswers": ["16 and 6", "15 and 6", "16 only", "15 only"],
+            "answer": 0,
+            "next_question": "2",
+            "client_response":""
+        },
+        "2":{
+            "quiz_id": "2",
+            "quiz_question": "What is the term for playing two hands when you're dealt two of the same card?",
+            "media":"https://www.playojo.com/blog/wp-content/uploads/2020/11/1-3-768x509.jpg",
+            "media_alt":"Quiz question 2 media alt text",
+            "panswers": ["Breaking", "Dividing", "Splitting", "Seperating"],
+            "answer": 2,
+            "next_question": "3",
+            "client_response":""
+        },
+        "3":{
+            "quiz_id": "3",
+            "quiz_question": "When is it good to take insurance?",
+            "media":"/static/assets/images/insurance.png",
+            "media_alt":"Quiz question 2 media alt text",
+            "panswers": ["Always take insurance", "Never take insurance", "Only if you have a bad hand", "When you're caught in a losing streak"],
+            "answer": 1,
+            "next_question": "4",
+            "client_response":""
+        },
+        "4":{
+            "quiz_id": "4",
+            "quiz_question": "What does a soft hand mean?",
+            "media":"/static/assets/images/insurance.png",
+            "media_alt":"Quiz question 2 media alt text",
+            "panswers": ["A hand with no face cards", "A bad hand", "A hand with an ace", "A hand with two identical cards"],
+            "answer": 2,
+            "next_question": "5",
+            "client_response":""
+        },
+        "5":{
+            "quiz_id": "5",
+            "quiz_question": "Should you surrender?",
+            "media":"/static/assets/images/surrender.png",
+            "media_alt":"Quiz question 2 media alt text",
+            "panswers": ["No, I should hit", "No, I should stand", "No, I should double", "Yes"],
+            "answer": 3,
+            "next_question": "6",
+            "client_response":""
+        }    
+    }
+}
